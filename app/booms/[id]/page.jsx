41da619b3b6f7735dcc137bfe6aaa49d1c4f01aa
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-10-08 14:24:13 +1300

    Deleting items

--- app/booms/[id]/page.jsx
+++ app/booms/[id]/page.jsx
@@ -3,17 +3,20 @@
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { ArrowLongLeftIcon } from "@heroicons/react/24/solid";
+import { ArrowLongLeftIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
 import { getBoom } from "@/app/_lib/actions_booms";
 import BoomDashboard from "@/app/_components/BoomDashboard";
+import { useTransition } from "react";
+import { revalidatePath } from "next/cache";
+import delete_item from "@/app/_lib/delete_item";
 
-const Page = (params) => {
+const Page = () => {
   const { id } = useParams();
   const [equipmentData, setEquipmentData] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     const fetchEquipmentData = async () => {
@@ -27,7 +30,7 @@ const Page = (params) => {
         setLoading(false);
       }
     };
-
+    
     if (equipmentData === null) {
       fetchEquipmentData();
     }
@@ -41,27 +44,50 @@ const Page = (params) => {
     );
   }
 
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this record?")) {
+      startTransition(async () => {
+        try {
+          await delete_item(equipmentData, "ws_booms");
+          // Optionally show success message or redirect user after deletion
+          revalidatePath("/booms")
+        } catch (error) {
+          console.error("Error deleting item:", error);
+          // Show error feedback to the user
+        }
+      });
+    }
+  }
+  
 
   return (
     <>
       {loading && <Spinner loading={loading} />}
       {!loading && equipmentData && (
-        <>
-          <Suspense fallback={<Spinner />}>
+        <Suspense fallback={<Spinner />}>
+          <div className="flex flex-row justify-between mb-2">
             <Link
               href="/booms"
-              className="flex items-centermb-4 text-2xl font-semibold text-primary-300"
+              className="flex items-center mb-4 text-2xl font-semibold text-primary-300"
             >
               {" "}
-              <span className="flex flex-row items-center gap-x-2 w-full text-xl font-medium">
-                <ArrowLongLeftIcon className="mr-2 h-6 w-6" /> Back to Booms
+              <span className="flex flex-row items-center w-full text-xl font-medium gap-x-2">
+                <ArrowLongLeftIcon className="w-6 h-6 mr-2" /> Back to Booms
               </span>
             </Link>
-
-            <BoomDashboard equipmentData={equipmentData} />
-          </Suspense>
-          
-        </>
+            <>
+              <button
+                title="Delete Item"
+                onClick={handleDelete}
+                disabled={isPending} 
+                className="flex text-2xl font-semibold items-centermb-4 text-primary-300"
+              >
+                <TrashIcon className="w-5 h-5 mt-2 transition-colors text-primary-600 group-hover:text-primary-100" />
+              </button>
+            </>
+          </div>
+          <BoomDashboard equipmentData={equipmentData} />
+        </Suspense>
       )}
     </>
   );
--- app/sails/[id]/page.jsx
+++ app/sails/[id]/page.jsx
@@ -3,19 +3,20 @@
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { ArrowLongLeftIcon } from "@heroicons/react/24/solid";
+import { ArrowLongLeftIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { Suspense } from "react";
-import { getMast } from "@/app/_lib/actions_masts";
-import DashboardComponent from "@/app/_components/Dashboard";
 import Spinner from "@/app/_components/Spinner";
-import MastDashboard from "@/app/_components/MastDashboard";
 import { getSail } from "@/app/_lib/actions_sails";
 import SailDashboard from "@/app/_components/SailDashboard";
+import { useTransition } from "react";
+import { revalidatePath } from "next/cache";
+import delete_item from "@/app/_lib/delete_item";
 
-const Page = (params) => {
+const Page = () => {
   const { id } = useParams();
   const [equipmentData, setEquipmentData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     const fetchEquipmentData = async () => {
@@ -43,27 +44,49 @@ const Page = (params) => {
     );
   }
 
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this record?")) {
+      startTransition(async () => {
+        try {
+          await delete_item(equipmentData, "ws_sails");
+          // Optionally show success message or redirect user after deletion
+          revalidatePath("/sails");
+        } catch (error) {
+          console.error("Error deleting item:", error);
+          // Show error feedback to the user
+        }
+      });
+    }
+  }
 
   return (
     <>
       {loading && <Spinner loading={loading} />}
       {!loading && equipmentData && (
-        <>
-          <Suspense fallback={<Spinner />}>
+        <Suspense fallback={<Spinner />}>
+          <div className="flex flex-row justify-between mb-2">
             <Link
               href="/sails"
-              className="flex items-centermb-4 text-2xl font-semibold text-primary-300"
+              className="flex items-center mb-4 text-2xl font-semibold text-primary-300"
             >
               {" "}
               <span className="flex flex-row items-center gap-x-2 w-full text-xl font-medium">
                 <ArrowLongLeftIcon className="mr-2 h-6 w-6" /> Back to Sails
               </span>
             </Link>
-
-            <SailDashboard equipmentData={equipmentData} />
-          </Suspense>
-          
-        </>
+            <>
+              <button
+                title="Delete Item"
+                onClick={handleDelete}
+                disabled={isPending}
+                className="flex items-center mb-4 text-2xl font-semibold text-primary-300"
+              >
+                <TrashIcon className="w-5 h-5 mt-2 transition-colors text-primary-600 group-hover:text-primary-100" />
+              </button>
+            </>
+          </div>
+          <SailDashboard equipmentData={equipmentData} />
+        </Suspense>
       )}
     </>
   );
