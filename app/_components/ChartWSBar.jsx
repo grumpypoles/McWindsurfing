515ef44a733e3e8e197c13dc3d52ff379b2a9177
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2026-09-22 09:14:56 +1200

    AG Grid Error Fixed

--- app/_components/ChartWSBar.jsx
+++ app/_components/ChartWSBar.jsx
@@ -1,22 +1,15 @@
 "use client";
 
-import { infiniteScrollHandler } from "@syncfusion/ej2-react-grids";
 import { AgCharts } from "ag-charts-react";
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-quartz.css";
 
 function ChartWSBar({ rowData }) {
-
   const chartOptions = {
     seriesDefaults: {},
     data: rowData,
     title: {
       text: "Ocean Activities - Frequency",
-      // color: ["#99B0C7"],
     },
     theme: "ag-vivid-dark",
-
-  
     series: [
       {
         type: "donut",
@@ -24,20 +17,11 @@ function ChartWSBar({ rowData }) {
         angleKey: "counter",
         innerRadiusRatio: 0.7,
         showInLegend: false,
-        // calloutLabel:false,
-    }
+      },
     ],
-
-    
   };
 
-  return (
-    <>
-      <div className="ag-theme-quartz-dark">
-        <AgCharts options={chartOptions} style={{ height: "600px" }} />
-      </div>
-    </>
-  );
+  return <AgCharts options={chartOptions} style={{ height: "600px" }} />;
 }
 
 export default ChartWSBar;
